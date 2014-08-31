e84e4e8f7570895623708b8c4a2a6b69b028ec11
Luke Bond <luke.n.bond@gmail.com>
2014-08-31 22:46:48 +0100

    Added customisable message via MESSAGE envvar

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var server = restify.createServer({
   name: 'demo'
 });
 
+var message = process.env.MESSAGE || 'Hello, world';
+
 server.get('/', function (req, res, next) {
-  res.send(200, 'Hello, world from ' + ip.address() + '!');
+  res.send(200, message + ' ' + ip.address() + '!');
   return next();
 });
 
