6ad27e88a59de3511725322838da9c2d63c90ff7
Luke Bond <luke.n.bond@gmail.com>
2018-08-15 22:49:38 +0100

    added version to output, bumped node version, added k8s yaml

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var ip = require('ip');
 var restify = require('restify');
+var pkg = require('./package.json');
 
 var server = restify.createServer({ name: 'demo' });
 
 var message = process.env.MESSAGE || 'Hello, world';
 
 server.get('/', function (req, res, next) {
-  res.send(200, message + ' ' + ip.address() + '!');
+  res.send(200, `${message} | ${ip.address()} | Version ${pkg.version}`);
   return next();
 });
 
