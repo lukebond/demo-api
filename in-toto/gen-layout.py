b2ce75e8ed817e4b5d0e37d9873e0092909e2196
Luke Bond <luke.n.bond@gmail.com>
2018-08-20 23:42:10 +0100

    adding in-toto stuff

---
+++ in-toto/gen-layout.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from in_toto.models.layout import Layout, Step
+from in_toto.models.metadata import Metablock
+from in_toto.util import generate_and_write_rsa_keypair, import_rsa_key_from_file
+
+generate_and_write_rsa_keypair("build_key")
+build_key = import_rsa_key_from_file("build_key.pub")
+
+layout = Layout()
+build = Step(name="build")
+build.expected_materials.append(['ALLOW', 'package.json'])
+build.expected_materials.append(['ALLOW', 'index.js'])
+build.expected_products.append(['CREATE', 'node_modules'])
+build.expected_command = ['npm', 'install']
+layout.steps.append(build)
+layout.add_functionary_key(build_key)
+
+build.pubkeys.append(build_key['keyid'])
+
+generate_and_write_rsa_keypair("root_key")
+root_key = import_rsa_key_from_file("root_key")
+
+metablock = Metablock(signed=layout)
+metablock.sign(root_key)
+metablock.dump("root.layout")
